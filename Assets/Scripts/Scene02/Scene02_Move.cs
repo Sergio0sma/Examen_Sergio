876dd1f1b4a299efdd1348a7be7316051a908f44
Sergio0sma <83583073+Sergio0sma@users.noreply.github.com>
2024-03-20 19:20:07 +0100

    Ejercicio 2 hecho

--- Assets/Scripts/Scene02/Scene02_Move.cs
+++ Assets/Scripts/Scene02/Scene02_Move.cs
@@ -2,8 +2,11 @@ using UnityEngine;
 
 public class Scene02_Move : MonoBehaviour
 {
+    private Rigidbody2D rb;
+
     private void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
@@ -13,5 +16,8 @@ public class Scene02_Move : MonoBehaviour
 
     private void Move()
     {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+        rb.velocity = new Vector2(horizontalInput, verticalInput) * 5f;
     }
 }
