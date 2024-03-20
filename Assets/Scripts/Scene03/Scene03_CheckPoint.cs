3da4b13fa6f005ee226b6661872ffcadbba6a91a
Sergio0sma <83583073+Sergio0sma@users.noreply.github.com>
2024-03-20 19:45:33 +0100

    Ejercicio 3 hecho

--- Assets/Scripts/Scene03/Scene03_CheckPoint.cs
+++ Assets/Scripts/Scene03/Scene03_CheckPoint.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Scene03_CheckPoint : MonoBehaviour
 {
-
+    
     void Start()
     {
     }
@@ -10,5 +10,16 @@ public class Scene03_CheckPoint : MonoBehaviour
 
     void Update()
     {
+       
     }
+
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+     Debug.Log("Nombre del checkpoint: "+ this.name);
+        //Cambiar el color del checkpoint
+        GetComponent<Renderer>().material.color = Color.green;
+    }
+
 }
