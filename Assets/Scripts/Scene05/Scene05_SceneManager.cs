181635220dda538f10863bd1b443e08fceeccec4
Sergio0sma <83583073+Sergio0sma@users.noreply.github.com>
2024-03-20 20:18:48 +0100

    Ejercicio 5 hecho

--- Assets/Scripts/Scene05/Scene05_SceneManager.cs
+++ Assets/Scripts/Scene05/Scene05_SceneManager.cs
@@ -6,5 +6,17 @@ public class Scene05_SceneManager : MonoBehaviour
     private void Awake()
     {
        
+       //Me quedo con la longitud de la array de objetos
+        int verdes = GameObject.FindGameObjectsWithTag("Verde").Length;
+        int rojos = GameObject.FindGameObjectsWithTag("Rojo").Length;
+
+        Debug.Log($"Circulos totales: {verdes + rojos}");
+        Debug.Log($"Circulos Verdes: {verdes}");
+        Debug.Log($"Circulos rojos: {rojos}");
+
+        //Me quedo con el numero de componentes que tienen colliders
+        int verdesColl = GameObject.FindGameObjectsWithTag("Verde").Where(Col => Col.GetComponent<Collider2D>() != null).Count();
+        int rojosColl = GameObject.FindGameObjectsWithTag("Rojo").Where(Col => Col.GetComponent<Collider2D>() != null).Count();
+        Debug.Log($"Circulos totales: {verdesColl + rojosColl}");
     }
 }
