ce6cb79f0c44e865720ec328edf75d2beab6ee8c
Sergio0sma <83583073+Sergio0sma@users.noreply.github.com>
2024-03-20 20:37:08 +0100

    Ejercicio 6 hecho

--- Assets/Scripts/Scene06/Scene06_SceneManager.cs
+++ Assets/Scripts/Scene06/Scene06_SceneManager.cs
@@ -4,8 +4,22 @@ using UnityEngine;
 
 public class Scene06_SceneManager : MonoBehaviour
 {
+    public GameObject[] interactables;
     private void Update()
     {
-
+        
+        if (Input.GetKey(KeyCode.E))
+        {
+            interactables = GameObject.FindGameObjectsWithTag("Bolas");
+            
+            foreach (GameObject bola in interactables)
+            {
+                if (bola.GetComponent<Interactable>())
+                {
+                    
+                    bola.GetComponent<SpriteRenderer>().color = Color.red;
+                }
+            }
+        }
     }
 }
